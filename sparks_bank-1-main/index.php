0f6cd4990ecc18bb1b9f28d5c90f3956cbd7f278
Anant Jain <ritujainanant2810@gmail.com>
2022-07-13 12:46:50 +0530

    First commit

---
+++ sparks_bank-1-main/index.php
@@ -0,0 +1,24 @@
+<?php include('nav.php') ?>
+
+
+  <h1>Welcome to Sparks Bank</h1>
+
+
+  <div class="bg">
+    <div class="center">
+      <ul>
+        <br>
+        <br>
+        <br>
+        <br>
+        <li class="operations" ><a href="transfer_money.php"><button class="btn" id="red"><h3>View All Users</h3></button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue"><h3>Trasnfer Money</h3></button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="green"><h3>View Transfer History</h3></button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
